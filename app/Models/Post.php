828a8f274baf5324cd905d0b8ae4734dc0a94374
NATNAEL BIRHANU <negusnati@gmail.com>
2022-12-04 10:11:47 -0800

    multiple filtering

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -32,6 +32,11 @@ class Post extends Model
             $query
                 ->where('title', 'like', '%' . $search . '%')
                 ->orWhere('body', 'like', '%' . $search . '%'));
+
+            $query->when($filters['catagory'] ?? false, fn ($query, $catagory) =>
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%'));
     }
 
     public function getRouteKeyName()
